8fc7b41c779e448aceb102fc49814f59aebc006d
Clynt Neiko Rupinta <31558769+Kiryuumaru@users.noreply.github.com>
2024-08-28 03:44:13 +0800

    Updated CICD (#1)
    * Added ignored
    
    * Updated CICD
    
    * Updated CICD 2
    
    * Updated CICD 3
    
    * Updated CICD 4

--- build/Build.cs
+++ build/Build.cs
@@ -1,8 +1,17 @@
+using Nuke.Common.IO;
 using NukeBuildHelpers;
 using NukeBuildHelpers.Common.Attributes;
 using NukeBuildHelpers.Entry;
 using NukeBuildHelpers.Entry.Extensions;
+using NukeBuildHelpers.RunContext.Interfaces;
 using NukeBuildHelpers.Runner.Abstraction;
+using System.Collections.Generic;
+using System;
+using NukeBuildHelpers.RunContext.Extensions;
+using Nuke.Common.Tools.DotNet;
+using NukeBuildHelpers.Common;
+using System.Drawing.Drawing2D;
+using System.Text.RegularExpressions;
 
 class Build : BaseNukeBuildHelpers
 {
@@ -12,30 +21,74 @@ class Build : BaseNukeBuildHelpers
 
     public override string MainEnvironmentBranch => "master";
 
+    private static readonly string[] archMatrix = ["x64", "arm64"];
+
     [SecretVariable("GITHUB_TOKEN")]
     readonly string? GithubToken;
 
-    BuildEntry BuildEntry => _ => _
-        .AppId("sample_app")
-        .RunnerOS(RunnerOS.Windows2022)
-        .Execute(() =>
+    BuildEntry BuildBinaries => _ => _
+        .AppId("managed-cicd-runner")
+        .Matrix(archMatrix, (definitionArch, arch) =>
         {
-            // build logic here
+            definitionArch.RunnerOS(RunnerOS.Windows2022);
+            definitionArch.WorkflowId($"build_windows_{arch}");
+            definitionArch.DisplayName($"[Build] Windows{arch.ToUpperInvariant()}");
+            definitionArch.ReleaseAsset(context => GetAssets(arch));
+            definitionArch.Execute(async context =>
+            {
+                var outAsset = GetOutAsset(arch);
+                var archivePath = outAsset.Parent / outAsset.NameWithoutExtension;
+                var outPath = archivePath / outAsset.NameWithoutExtension;
+                var proj = RootDirectory / "src" / "Presentation" / "Presentation.csproj";
+                DotNetTasks.DotNetBuild(_ => _
+                    .SetProjectFile(proj)
+                    .SetConfiguration("Release"));
+                DotNetTasks.DotNetPublish(_ => _
+                    .SetProject(proj)
+                    .SetConfiguration("Release")
+                    .EnableSelfContained()
+                    .SetRuntime($"win-{arch.ToLowerInvariant()}")
+                    .EnablePublishSingleFile()
+                    .SetOutput(outPath));
+
+                await (outPath / "Presentation.exe").MoveRecursively(outPath / "HyperVCompose.exe");
+
+                archivePath.ZipTo(outAsset);
+
+                if (context.TryGetVersionedContext(out var versioned))
+                {
+                    (OutputDirectory / $"installer_{arch}.ps1").WriteAllText((RootDirectory / "installerTemplate.ps1").ReadAllText()
+                        .Replace("{{$repo}}", "Kiryuumaru/ManagedCICDRunner")
+                        .Replace("{{$appname}}", $"HyperVCompose_Windows{arch.ToUpperInvariant()}")
+                        .Replace("{{$appexec}}", "HyperVCompose.exe")
+                        .Replace("{{$rootextract}}", $"HyperVCompose_Windows{arch.ToUpperInvariant()}"));
+                }
+            });
         });
 
-    TestEntry TestEntry => _ => _
-        .AppId("sample_app")
-        .RunnerOS(RunnerOS.Ubuntu2204)
-        .Execute(() =>
+    string GetVersion(IRunContext context)
+    {
+        string version = "0.0.0";
+        if (context.TryGetVersionedContext(out var versionedContext))
         {
-            // test logic here
-        });
+            version = versionedContext.AppVersion.Version.ToString();
+        }
+        return version;
+    }
 
-    PublishEntry PublishEntry => _ => _
-        .AppId("sample_app")
-        .RunnerOS(RunnerOS.Ubuntu2204)
-        .Execute(context =>
-        {
-            // publish logic here
-        });
+    AbsolutePath GetOutAsset(string arch)
+    {
+        return OutputDirectory / ($"HyperVCompose_Windows{arch.ToUpperInvariant()}.zip");
+    }
+
+    AbsolutePath[] GetAssets(string arch)
+    {
+        List<AbsolutePath> assets = [];
+
+        assets.Add(GetOutAsset(arch));
+
+        assets.Add(OutputDirectory / $"installer_{arch}.ps1");
+
+        return [.. assets];
+    }
 }
--- src/Presentation/Controllers/WeatherForecastController.cs
+++ src/Presentation/Controllers/WeatherForecastController.cs
@@ -2,6 +2,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.Controllers
 {
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
