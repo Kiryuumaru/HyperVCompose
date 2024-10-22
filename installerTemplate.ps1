321eb32c3b2ce5e85b836951aa638e031c2adee6
Clynt Neiko Rupinta <neiko@meldcx.com>
2024-10-22 18:54:42 +0800

    Updated CICD

--- installerTemplate.ps1
+++ installerTemplate.ps1
@@ -30,6 +30,6 @@ Expand-Archive -LiteralPath $appZipPath -DestinationPath $tempPath -Force
 $HVC_HOME = "$env:ProgramData\hvc"
 
 if ($string -notlike "*$HVC_HOME*") {
-    $env:PATH = $env:PATH + ";$HVC_HOME\\;$HVC_HOME";
-    Set-ItemProperty -Path 'HKLM:\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment\\' -Name Path -Value $env:PATH
+    $env:PATH = $env:PATH + ";$HVC_HOME\;$HVC_HOME";
+    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment\' -Name Path -Value $env:PATH
 }
